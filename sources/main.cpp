0bc2b94499ffb5204d6abbe0dd8487ae7ded0e57
German <germangolozubow@gmail.com>
2024-03-08 13:18:57 +0300

    Added initial random test С++ files and code
    The main code calling the tests is now C++.
    A random test is also C++, this is necessary
    in order to check the hash table using std::map.

---
+++ sources/main.cpp
@@ -0,0 +1,20 @@
+//#include "tests/tests.h"
+#include "tests/random_test.hpp"
+#include <cstdio>
+
+extern "C" { void test_put_and_get(); }
+extern "C" { void test_put_and_get_rehash(); }
+extern "C" { void test_delete(); }
+
+int main(void)
+{
+    //printf("test_put_and_get()\n");
+    //test_put_and_get();
+    //printf("test_put_and_get_rehash()\n");
+    //test_put_and_get_rehash();
+    //printf("test_delete()\n");
+    //test_delete();
+    printf("test_random()\n");
+    test_random();
+    return 0;
+}
